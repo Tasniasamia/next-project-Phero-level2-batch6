87376815256dcbc81b9d7dcb5c2772d34b8fd6ca
tasniaarhamarham <tasniaarhamarham@gmail.com>
2026-01-27 01:30:41 +0600

    pagination full on setup

--- src/components/common/pagination.tsx
+++ src/components/common/pagination.tsx
@@ -15,8 +15,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { ArrowLeft } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
+import { Button } from "../ui/button";
 
 type metaType = {
   limit: number;
@@ -24,32 +26,38 @@ type metaType = {
   total: number;
   totalPages: number;
 };
-export  function PaginationCustom({meta}: {meta:metaType}) {
-    const route=useRouter();
-    const[value,setValue]=useState<string|number>(10)
+export function PaginationCustom({ meta }: { meta: metaType }) {
+  const route = useRouter();
+  const { limit: pageSize, page: currentPage, total, totalPages } = meta;
+
+  const [value, setValue] = useState<string | number>(pageSize.toString());
   console.log("meta", meta);
   const searchParams = useSearchParams();
-  const {limit:pageSize,page:currentPage,total,totalPages}=meta;
-  const naviateToPage = (page:number) => {
-    const params=new URLSearchParams(searchParams.toString());
-    params.set('page',page.toString());
-    params.set('limit',value?.toString());
+  const naviateToPage = (page: number,limit:number) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("page", page.toString() || "1");
+    params.set("limit", limit?.toString() || pageSize.toString());
     route.push(`?${params.toString()}`);
   };
   return (
     <div className="flex items-center justify-between gap-4">
       <Field orientation="horizontal" className="w-fit">
         <FieldLabel htmlFor="select-rows-per-page">Rows per page</FieldLabel>
-        <Select   defaultValue={pageSize.toString()}
-  onValueChange={(value) => {
-    setValue(value)
-  }}>
+        <Select
+          defaultValue={pageSize.toString()}
+          onValueChange={(value) => {
+            console.log("select value", value);
+            setValue(value);
+            naviateToPage(currentPage,Number(value));
+          }}
+        >
           <SelectTrigger className="w-20" id="select-rows-per-page">
             <SelectValue />
           </SelectTrigger>
           <SelectContent align="start">
             <SelectGroup>
-              <SelectItem value="10">10</SelectItem>
+              <SelectItem value="8">8</SelectItem>
+             <SelectItem value="10">10</SelectItem>
               <SelectItem value="25">25</SelectItem>
               <SelectItem value="50">50</SelectItem>
               <SelectItem value="100">100</SelectItem>
@@ -60,10 +68,30 @@ export  function PaginationCustom({meta}: {meta:metaType}) {
       <Pagination className="mx-0 w-auto">
         <PaginationContent>
           <PaginationItem>
-            <PaginationPrevious onClick={()=>{let page; naviateToPage(page=currentPage+1)}} aria-disabled={currentPage==1}/>
+            <Button
+              className="cursor-pointer"
+              onClick={() => {
+                let page = Number(currentPage);
+                console.log(page);
+                naviateToPage((page = page - 1),Number(value));
+              }}
+              disabled={currentPage == 1}
+            >
+              Previous
+            </Button>
           </PaginationItem>
           <PaginationItem>
-            <PaginationNext onClick={()=>{let page; naviateToPage(page=currentPage-1)}} aria-disabled={currentPage==totalPages}/>
+            <Button
+              className="cursor-pointer"
+              onClick={() => {
+                let page = Number(currentPage);
+                console.log(page);
+                naviateToPage((page = page + 1),Number(value));
+              }}
+              disabled={currentPage == totalPages}
+            >
+              Next
+            </Button>
           </PaginationItem>
         </PaginationContent>
       </Pagination>
